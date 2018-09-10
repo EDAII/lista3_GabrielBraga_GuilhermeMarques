f7949288207d6a95bb7054094dd3ba94e7a762ca
Guilherme Marques <marquesguilherme3@gmail.com>
2018-09-10 17:22:53 -0300

    Implements quicksort

--- sorters.py
+++ sorters.py
@@ -1,5 +1,30 @@
 import time
+from math import floor
 
 class Sorter(object):
     def __init__(self):
         pass
+
+    def timer(self, func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        elapsed = end - start
+
+        return (elapsed, result)
+
+    def quicksort(self, l, start, end):
+        if start < end:
+            i = start
+            j = end
+            indexPivot = floor((start + end)/2)
+            pivot = l[indexPivot]
+            while (i < j):
+                while (l[i] < pivot):
+                    i += 1
+                while (l[j] > pivot):
+                    j -= 1
+                if (i <= j):
+                    l[i], l[j] = l[j], l[i]
+            self.quicksort(l, start, indexPivot)
+            self.quicksort(l, indexPivot+1, end)
